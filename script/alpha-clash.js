32965211afb0b3e41b9862ab558f055357a2aeab
rakib bari <rakibbari@rakibs-MacBook-Pro.local>
2024-06-11 02:40:38 +0600

    finally done my project

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,20 +1,67 @@
 function play() {
-  //   const homeScreenSection = document.getElementById("home-screen");
-  //   homeScreenSection.classList.add("hidden");
-
-  //   const playGroundSection = document.getElementById("play-ground");
-  //   playGroundSection.classList.remove("hidden");
-
   hideElement("home-screen");
+  hideElement("final-score");
   showElement("play-ground");
+  //reset score and life
+  setElementById("current-life", 5);
+  setElementById("current-score", 0);
+
   continueGame();
+}
+
+function gameOver() {
+  hideElement("play-ground");
+  showElement("final-score");
+
+  // show the gameover result
+  const lastScore = getElementById("current-score");
+  setElementById("last-score", lastScore);
+
+  //clar all background
+  const currentAlpha = getTextElementById("current-alpha");
+  removeBacgroundColor(currentAlpha);
 }
 
 function continueGame() {
   const alphabets = getRandomAlphabet();
-  console.log("random alphabet : ", alphabets);
+  // console.log("random alphabet : ", alphabets);
 
   const currentAlpha = document.getElementById("current-alpha");
   currentAlpha.innerHTML = alphabets;
   setBacgroundColor(alphabets);
 }
+function keyboardButtonPress(event) {
+  //player click
+  const playerPressed = event.key;
+
+  if (playerPressed === "Escape") {
+    gameOver();
+  }
+  //current alpha
+  const currentAlphabet = document.getElementById("current-alpha");
+  const alphaNow = currentAlphabet.innerHTML;
+  const expectedAlpha = alphaNow.toLowerCase();
+  // console.log(expectedAlpha, playerPressed);
+
+  if (expectedAlpha === playerPressed) {
+    console.log("you get a point");
+    //score
+    const currentScore = getElementById("current-score");
+    const newscore = currentScore + 1;
+    setElementById("current-score", newscore);
+
+    removeBacgroundColor(expectedAlpha);
+    continueGame();
+  } else {
+    console.log("lose your life line");
+    const currentScore = getElementById("current-life");
+    const newLife = currentScore - 1;
+    setElementById("current-life", newLife);
+
+    if (newLife === 0) {
+      gameOver();
+    }
+  }
+}
+
+document.addEventListener("keyup", keyboardButtonPress);
--- script/utility.js
+++ script/utility.js
@@ -17,8 +17,28 @@ function getRandomAlphabet() {
   const alphabet = alphabets[alphaNumber];
   return alphabet;
 }
+function getTextElementById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = element.innerHTML;
+  return elementText;
+}
+
+function getElementById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = element.innerHTML;
+  const value = parseInt(elementText);
+  return value;
+}
+function setElementById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerHTML = value;
+}
 
 function setBacgroundColor(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add("bg-orange-400");
+}
+function removeBacgroundColor(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
 }
